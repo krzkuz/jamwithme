a04ab33903e2234fa02db56838e677d9b6ea2a46
krzkuz <107817601+krzkuz@users.noreply.github.com>
2022-11-12 21:28:43 +0100

    Delete users directory

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Instrument, Follow
-# Register your models here.
-    
-admin.site.register(Profile)
-admin.site.register(Instrument)
-admin.site.register(Follow)
-
-
