971d6978e972df0187e541baa887f25409d6bffd
krzkuz <krzkuz1@gmail.com>
2022-11-22 19:17:40 +0100

    production

--- jamwithme/settings.py
+++ jamwithme/settings.py
@@ -70,6 +70,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://jwm95.herokuapp.com']
+
 ROOT_URLCONF = "jamwithme.urls"
 
 TEMPLATES = [
